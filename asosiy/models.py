8def9577686ddad9be59217f93c2cc2b0d3c48d9
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2023-11-30 21:33:21 -0800

    2

---
+++ asosiy/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Muallif(models.Model):
+    ism = models.CharField(max_length=30)
+    yosh = models.PositiveSmallIntegerField(blank=True)
+    kasbi = models.CharField(max_length=50, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Maqola(models.Model):
+    sarlavha = models.CharField(max_length=50)
+    sana = models.DateField(blank=True, null=True)
+    mavzu = models.CharField(max_length=50, blank=True)
+    matn = models.TextField()
+    muallif = models.ForeignKey(Muallif, on_delete=models.SET_NULL, null=True)
